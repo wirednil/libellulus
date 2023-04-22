1b198a107094b9dd8eca6a4485ece04a3447a23e
Diogenes Cristaldo <diogenescristaldo@gmail.com>
2023-04-22 17:59:19 -0300

    Committing documentation changes.

--- inc/Logger/WLogger.h
+++ inc/Logger/WLogger.h
@@ -1,13 +1,42 @@
+/**
+ *
+ * @file WLogger.h
+ * @brief Header file containing function declarations for WLogger, a C-style logging library.
+ * WLogger provides C-style logging functions that can be used to log messages with varying levels of severity.
+ * The header file defines the wrapper_logger() function, which can be called to log messages.
+ *
+ */
 #ifndef _WLOGGER_H_
 #define _WLOGGER_H_
 
-#include "Logger.hpp"
+#include <stdarg.h>
+#include "macros.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ *
+ * @brief Logs a message with a specified severity level, function name, file name, line number and format string.
+ * This function logs a message with a specified severity level, function name, file name, line number and format string.
+ * The format string supports placeholders, which can be replaced with the supplied arguments.
+ *
+ * @param level The severity level of the message to be logged.
+ * @param func The name of the function where the log message is being called from.
+ * @param file The name of the file where the log message is being called from.
+ * @param line The line number where the log message is being called from.
+ * @param fmt The format string for the log message.
+ * @param ... Optional arguments to be replaced in the format string placeholders.
+ * 
+ * @return void.
+ *
+ */
 void wrapper_logger(Level level,
-               const char* fmt,
-               const char* file,
-               int line,
-               const char* func,
-               va_list args);
+    const char* func, const char* file, int line, const char* fmt, ...);
 
-#endif //_WLOGGER_H_
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _WLOGGER_H_
--- inc/SimpleLog.h
+++ inc/SimpleLog.h
@@ -1,24 +1,83 @@
+/**
+ * @file SimpleLog.h
+ * @brief Defines macros for logging messages at different levels of severity using a wrapper around WLogger.
+ */
+
 #ifndef __SIMPLE_LOG_H__
 #define __SIMPLE_LOG_H__
+#include <stdlib.h>
+#include <stdbool.h>
+#include "macros.h"
+#include "WLogger.h"
 
+/**
+ * @brief Logs a message at the DEBUG level using a wrapper around WLogger.
+ *
+ * The SIMPLE_DEBUG macro logs a message at the DEBUG level using a wrapper around WLogger. It accepts a printf-style
+ * format string and zero or more additional arguments to be formatted and included in the log message. The macro
+ * automatically includes information about the file, line number, and function where it was called.
+ *
+ * @param FMT The printf-style format string for the log message.
+ * @param ... Zero or more additional arguments to be included in the log message.
+ */
 #define SIMPLE_DEBUG(FMT, ...) do { \
-    wrapper_logger(DEBUG, FMT, __FILE__, __LINE__, __func__, ##args); \
+    wrapper_logger(DEBUG, __func__, __FILE__, __LINE__, FMT, ##__VA_ARGS__); \
 } while (false)
 
+/**
+ * @brief Logs a message at the INFO level using a wrapper around WLogger.
+ *
+ * The SIMPLE_INFO macro logs a message at the INFO level using a wrapper around WLogger. It accepts a printf-style
+ * format string and zero or more additional arguments to be formatted and included in the log message. The macro
+ * automatically includes information about the file, line number, and function where it was called.
+ *
+ * @param FMT The printf-style format string for the log message.
+ * @param ... Zero or more additional arguments to be included in the log message.
+ */
 #define SIMPLE_INFO(FMT, ...) do { \
-    wrapper_logger(INFO, FMT, __FILE__, __LINE__, __func__, ##args); \
+    wrapper_logger(INFO, __func__, __FILE__, __LINE__, FMT, ##__VA_ARGS__); \
 } while (false)
 
+/**
+ * @brief Logs a message at the ERROR level using a wrapper around WLogger.
+ *
+ * The SIMPLE_ERROR macro logs a message at the ERROR level using a wrapper around WLogger. It accepts a printf-style
+ * format string and zero or more additional arguments to be formatted and included in the log message. The macro
+ * automatically includes information about the file, line number, and function where it was called.
+ *
+ * @param FMT The printf-style format string for the log message.
+ * @param ... Zero or more additional arguments to be included in the log message.
+ */
 #define SIMPLE_ERROR(FMT, ...) do { \
-    wrapper_logger(ERROR, FMT, __FILE__, __LINE__, __func__, ##args); \
+    wrapper_logger(ERROR, __func__, __FILE__, __LINE__, FMT, ##__VA_ARGS__); \
 } while (false)
 
+/**
+ * @brief Logs a message at the WARNING level using a wrapper around WLogger.
+ *
+ * The SIMPLE_WARNING macro logs a message at the WARNING level using a wrapper around WLogger. It accepts a printf-style
+ * format string and zero or more additional arguments to be formatted and included in the log message. The macro
+ * automatically includes information about the file, line number, and function where it was called.
+ *
+ * @param FMT The printf-style format string for the log message.
+ * @param ... Zero or more additional arguments to be included in the log message.
+ */
 #define SIMPLE_WARNING(FMT, ...) do { \
-    wrapper_logger(WARNING, FMT, __FILE__, __LINE__, __func__, ##args); \
+    wrapper_logger(WARNING, __func__, __FILE__, __LINE__, FMT, ##__VA_ARGS__); \
 } while (false)
 
-#define SIMPLE_FATAL(FMT, ...) do { \
-    wrapper_logger(FATAL, FMT, __FILE__, __LINE__, __func__, ##args); \
+/**
+ * @brief Logs a message at the FATAL level using a wrapper around WLogger.
+ *
+ * The SIMPLE_FATAL macro logs a message at the FATAL level using a wrapper around WLogger. It accepts a printf-style
+ * format string and zero or more additional arguments to be formatted and included in the log message. The macro
+ * automatically includes information about the file, line number, and function where it was called.
+ *
+ * @param FMT The printf-style format string for the log message.
+ * @param ... Zero or more additional arguments to be included in the log message.
+ */
+ #define SIMPLE_FATAL(FMT, ...) do { \
+    wrapper_logger(FATAL, __func__, __FILE__, __LINE__, FMT, ##__VA_ARGS__); \
 } while (false)
 
-#endif //__SIMPLE_LOG_H__
+#endif //SIMPLE_LOG_H
